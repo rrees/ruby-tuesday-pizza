6c8f460a4abdf32a798d382f45b9c01cb553d6ef
Robert Rees <rrees@the-mac-beast.local>
2008-10-05 12:55:46 +0100

    Added a specification for what the controller should do when people order pizza

---
+++ spec/controller_spec.rb
@@ -0,0 +1,8 @@
+require "#{File.dirname(__FILE__)}/../app/main_controller.rb"
+
+
+describe MainController do
+  
+  it "should store someone's name when they confirm they are attending"
+  it "should store someone's pizza choice when they confirm the are attending"
+end
