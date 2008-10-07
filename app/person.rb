7e731229e7e72911835a51571448b1933203e02f
Carlos Villela <cv@lixo.org>
2008-10-07 19:19:56 +0100

    added names to people

--- app/person.rb
+++ app/person.rb
@@ -1,6 +1,13 @@
-
 class Person
+
+  def initialize(fields={})
+    @name = fields.delete(:name) or raise 'Name missing'
+  end
+
   def attends(tuesday)
     tuesday.attendees << self
   end
+
+  attr_reader :name
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,7 +4,7 @@ describe Person do
   
   it "should attend a Tuesday" do
     # Given
-    person = Person.new
+    person = Person.new :name => "Carlos"
     tuesday = Tuesday.next
     attendees = []
     tuesday.should_receive(:attendees).and_return(attendees)
@@ -14,5 +14,15 @@ describe Person do
     
     # Then
     attendees.should include(person)
+  end
+  
+  it "should have a name" do
+    person = Person.new :name => "Danilo"
+    
+    person.name.should == "Danilo"
+  end
+  
+  it "should not allow empty names" do
+    proc { Person.new }.should raise_error
   end
 end
