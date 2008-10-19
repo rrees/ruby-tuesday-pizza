ab35f8b780358defa53b4cd85a1164b55f200e73
Robert Rees <rrees@the-mac-beast.local>
2008-10-19 21:41:07 +0100

    Emptied pizza.yaml

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -5,5 +5,6 @@ describe MainController do
   it "should store someone's name when they confirm they are attending"
   it "should store someone's pizza choice when they confirm the are attending"
   
-  it 
-end
+  it "should store a confirmation that someone is coming to Ruby Tuesday"
+ 
+ end
